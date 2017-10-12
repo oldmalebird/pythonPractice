0fbc1249678419c87e90bf87406dd22222e8d857
oldmalebird <oldmalebird@users.noreply.github.com>
2017-10-12 22:08:12 +1300

    crawl wechat, failed to use PIL
    Installed Pillow successfully, but can't import PIL.
    Installed image, but has no attribute 'open'

--- project01.crawlWechat.py
+++ project01.crawlWechat.py
@@ -1,4 +1,4 @@
-'''爬取自己的微信，首先安装itchat'''
+'''爬取自己的微信，首先安装itchat、结巴分词jieba、wordcloud'''
 import itchat
 itchat.login()
 #爬取自己好友相关信息，返回一个json文件
@@ -17,3 +17,48 @@ for i in friends[1:]:
 #计算朋友总数
 total = len(friends[1:])
 print('Male: %.2f%%'%(float(male)/total*100)+'\n'+'Female: %.2f%%'%(float(female)/total*100)+'\n'+'Other: %.2f%%'%(float(other)/total*100)+'\n')
+#print(friends)
+#定义一个函数，用来爬取各个变量
+def get_var(var):
+    variable = []
+    for i in friends:
+        value = i[var]
+        variable.append(value)
+    return variable
+#调用函数得到各变量，并把数据存到csv文件中，保存到桌面
+NickName = get_var('NickName')
+Sex = get_var('Sex')
+Province = get_var('Province')
+City = get_var('City')
+Signature = get_var('Signature')
+from pandas import DataFrame
+data = {'NickName':NickName,'Sex':Sex, 'Province':Province, 'City':City, 'Signature': Signature}
+frame = DataFrame(data)
+frame.to_csv('data.csv', index = True)
+#制作词云
+import re
+siglist = []
+for i in friends:
+    signature = i['Signature'].strip().replace('span','').replace('class','').replace('emoji','')
+    rep = re.compile('1f\d+\w*|[<>/=}]')
+    signature = rep.sub('',signature)
+    siglist.append(signature)
+text = ''.join(siglist)
+#画词云
+import jieba
+wordlist = jieba.cut(text,cut_all = True)
+word_space_split = ''.join(wordlist)
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud, ImageColorGenerator
+import numpy as np
+import Image
+coloring = np.array(Image.open("/Users/apple/Desktop/wechat.jpg"))
+my_wordcloud = WordCloud(background_color="white", max_words=2000,
+                         mask=coloring, max_font_size=60, random_state=42, scale=2,
+                         font_path="/Library/Fonts/Microsoft/SimHei.ttf").generate(word_space_split)
+
+image_colors = ImageColorGenerator(coloring)
+plt.imshow(my_wordcloud.recolor(color_func=image_colors))
+plt.imshow(my_wordcloud)
+plt.axis("off")
+plt.show()
