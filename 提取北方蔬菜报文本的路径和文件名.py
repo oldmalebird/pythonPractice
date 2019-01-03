b3e967f8ae1c1004908b8129b5391cd85ff6e4f1
oldmalebird <lgnlgnlgnlgn@gmail.com>
2019-01-03 16:03:34 +0800

    增加txt文件的路径名列表

--- 提取北方蔬菜报文本的路径和文件名.py
+++ 提取北方蔬菜报文本的路径和文件名.py
@@ -1,9 +1,10 @@
 #读取filelist.txt，如果该行结尾为.txt，则把该路径存储到dirList里面，把文件名存储到titleList里面
 
 import re
-
 #建立北方蔬菜报文件地址空列表
 dirList = []
+#建立北方蔬菜报txt文件地址空列表
+dirTxtList = []
 #建立北方蔬菜报文件名空列表
 titleFileList = []
 #建立北方蔬菜报标题空列表
@@ -18,7 +19,7 @@ with open(r"D:\Data\小程序\北方蔬菜报\filelist.txt") as dirfile:
         dirList.append(line)
         if line.endswith('.txt'):
             # print(line)
-            dirList.append(line)
+            dirTxtList.append(line)
             title = line.split('\\')
             # print('1', title)
             # print('2', line)
@@ -29,6 +30,7 @@ with open(r"D:\Data\小程序\北方蔬菜报\filelist.txt") as dirfile:
             titleList.append(title)
             print(title)
 
-#print(dirList)
-# for i in range(len(titleList)):
-#     print(i, ":", titleList[i])
+# print(dirTxtList)
+# 按行打印所有txt文件的路径
+# for i in range(len(dirList)):
+#     print(dirList[i])
