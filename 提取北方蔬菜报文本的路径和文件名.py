801bf2dc25ac38ae21cdfe9538c14894996ea3e6
oldmalebird <lgnlgnlgnlgn@gmail.com>
2019-01-03 17:07:47 +0800

    合并不同文件夹的txt未遂

--- 提取北方蔬菜报文本的路径和文件名.py
+++ 提取北方蔬菜报文本的路径和文件名.py
@@ -1,5 +1,6 @@
+# -*- coding:utf-8*-
 #读取filelist.txt，如果该行结尾为.txt，则把该路径存储到dirList里面，把文件名存储到titleList里面
-
+import os
 import re
 #建立北方蔬菜报文件地址空列表
 dirList = []
@@ -32,5 +33,33 @@ with open(r"D:\Data\小程序\北方蔬菜报\filelist.txt") as dirfile:
 
 # print(dirTxtList)
 # 按行打印所有txt文件的路径
-# for i in range(len(dirList)):
-#     print(dirList[i])
+# for i in range(len(dirTxtList)):
+print('finish part 1.')
+'''
+# 把所有txt文件都合并到all.txt文件中
+all = open(r"D:\Data\小程序\北方蔬菜报\2017\all.txt", 'a+', encoding='UTF-8')
+print('finish part 2.')
+for i in range(len(dirTxtList)):
+    # 增加rb后无UnicodeDecodeError: 'gbk' codec can't decode byte 0xa4 in position 16
+    f = open(dirTxtList[i], 'rb')
+    #变成str才可以用encode，然后还要把byte还原为str，才可以与\n相连
+    all.write(str(str(f.read()).encode('utf-8')) + '\n')
+    #all.write(f.read())
+    #f.read()
+    print('finish read')
+'''
+# 把所有txt文件都合并到all.txt文件中
+all = open(r"D:\Data\小程序\北方蔬菜报\2017\all.txt", 'w', encoding='UTF-8')
+print('finish part 2.')
+for line in open(r"D:\Data\小程序\北方蔬菜报\txt文件地址.txt"):
+    # 增加rb后无UnicodeDecodeError: 'gbk' codec can't decode byte 0xa4 in position 16
+    address = line.strip()
+    print(address)
+    f = open(address.strip())
+    #变成str才可以用encode，然后还要把byte还原为str，才可以与\n相连
+    all.write(str(str(f.read()).encode('utf-8')) + '\n')
+    #all.write(f.read())
+    #f.read()
+    print('finish read')
+
+all.close()
