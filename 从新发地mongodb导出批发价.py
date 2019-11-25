8c1e8edbeb1fc0b202aa463db628a5bead9006cb
oldmalebird <lgnlgnlgnlgn@gmail.com>
2019-11-25 13:24:51 +0800

    Create 从新发地mongodb导出批发价.py

---
+++ 从新发地mongodb导出批发价.py
@@ -0,0 +1,16 @@
+from pymongo import MongoClient
+import pymongo
+import pandas as pd
+import numpy as np
+
+client = MongoClient()  # mongodb server
+db = client['xfd_db']
+df = db['xfd_veg_price']
+#xfd_veg_price = client.xfd_db.xfd_veg_price
+#x=db.getCollection('xfd_veg_price').find({"date":{$gte:"2019-01-01"}})
+#xfd_veg_price.find({"date":{$gte:"2019-01-01"}})
+data = pd.DataFrame(list(df.find()))
+
+writer = r"D:\Desktop\test.csv"
+data.to_csv(writer, index=False)
+#打开时需要从excel获取外部数据导入，编码选择utf-8
