6a54adf766dcbd635fa72ce7b02ab56aa873d751
oldmalebird <lgnlgnlgnlgn@gmail.com>
2020-05-13 17:47:35 +0800

    Update 合并2018年分省分品种面积数据.py

--- 合并2018年分省分品种面积数据.py
+++ 合并2018年分省分品种面积数据.py
@@ -6,7 +6,6 @@ docAddress = r"D:\Data\2019价格监测系统\历史数据\季度累计播种面
 areaDocList = os.listdir(docAddress)
 print(areaDocList)
 #提取第一个文件
-#添加文件名列
 df = pd.read_excel(
     r"D:\Data\2019价格监测系统\历史数据\季度累计播种面积\2018季度累计播种面积\2018-1-内蒙古自治区.xlsx",
     #sheet_name='（三）主要蔬菜面积季度调查表',
@@ -18,7 +17,10 @@ df = pd.read_excel(
     ],
     skiprows=20,
     skipfooter=7)
-df["文件名"] = "2018-1-内蒙古自治区"
+#添加文件名列
+df["年"] = "2018"
+df['季度'] = "1"
+df['省/直辖市'] = "内蒙古自治区"
 
 print(df.head(10))
 print(df.tail(10))
@@ -27,10 +29,10 @@ print('读取的df：', len(df.index))
 #提取其他文件
 for i in range(1, len(areaDocList)):
     docName = areaDocList[i]
-    doc_address = r"D:\Data\2019价格监测系统\历史数据\季度累计播种面积\2018季度累计播种面积"
-    doc_address += '\\' + docName
+    docAddress = r"D:\Data\2019价格监测系统\历史数据\季度累计播种面积\2018季度累计播种面积"
+    docAddress += '\\' + docName
     print("本次打开的文件名为：", docName)
-    print("本次打开的文件路径为：", doc_address)
+    print("本次打开的文件路径为：", docAddress)
     df_temp = pd.read_excel(
         docAddress,
         #sheet_name='（三）主要蔬菜面积季度调查表',
@@ -43,9 +45,12 @@ for i in range(1, len(areaDocList)):
         ],
         skiprows=20,
         skipfooter=7)
-    df_temp["文件名"] = docName
+
+    df_temp["年"] = docName.split(".")[0].split('-')[0]
+    df_temp["季度"] = docName.split(".")[0].split('-')[1]
+    df_temp["省/直辖市"] = docName.split(".")[0].split('-')[2]
     df = df.append(df_temp)
-    print("第" + i + "个文件append成功")
+    print("第" + str(i) + "个文件append成功")
 
 writer = r"D:\Data\2019价格监测系统\历史数据\季度累计播种面积\2018季度累计播种面积.xlsx"
 df.to_excel(writer, sheet_name="季度累计播种面积", index=False)
